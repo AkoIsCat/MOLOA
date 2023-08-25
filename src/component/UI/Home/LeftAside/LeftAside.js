2c7969b25e7069f8d82697432843098ecbeac588
KimEunHye <gpqjr00@naver.com>
2023-08-25 17:00:07 +0900

    ReFactor: api 임시 적용

--- src/component/UI/Home/LeftAside/LeftAside.js
+++ src/component/UI/Home/LeftAside/LeftAside.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import React, { useEffect, useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
+import { getNotificationList } from '../../../../api/LostarkAxios';
+import { getFirebaseData } from '../../../../api/FirebaseAxios';
 
 import { HiOutlineSpeakerphone } from 'react-icons/hi';
 import ContentBox from './ContentBox';
@@ -8,8 +10,6 @@ import SmallMenu from '../../SmallMenu';
 
 const lostArkKey =
   'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyIsImtpZCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyJ9.eyJpc3MiOiJodHRwczovL2x1ZHkuZ2FtZS5vbnN0b3ZlLmNvbSIsImF1ZCI6Imh0dHBzOi8vbHVkeS5nYW1lLm9uc3RvdmUuY29tL3Jlc291cmNlcyIsImNsaWVudF9pZCI6IjEwMDAwMDAwMDAwMjc0MTYifQ.MIy7jDe9w81yjIX8Zh4VgGCVH2IR-vz7CGF6Ceh0zdc-5HfnY31XrIwJ86r_nz1ImkS-dPxW7bO_8AaZmuII6sbdJo_dWer-kbkpA5kx1aIrtGqpvhY_fWtXY-_wmWhZrdAFJTtB8t6yVHIua_ceA7CJWM0Bn1sQ6SNWxCbq9fsHb6BGRayKuJ5JV-qAIVC5VjNyVC4iIyAdJetDWgu0c7DTR_pVOeWHbsX-CbAqqKXvRPoNII1aop4Ioa9Sbhb99iD-BuA7pfn-_D-m6axvO0-0luLu4UbwXhrE5jEVPNs7Oxf215AqosVjFb5ObX74iGzf6vyt8YqjL08UkLS8NQ';
-
-const MoloaNotiUrl = `https://lostark-bf0ba-default-rtdb.firebaseio.com/MoloaNoti.json`;
 
 const LeftAside = () => {
   const [noti, setNoti] = useState([]);
@@ -32,16 +32,18 @@ const LeftAside = () => {
   useEffect(() => {
     const loadLostApi = async () => {
       try {
-        const response = await fetch(
-          'https://developer-lostark.game.onstove.com/news/notices?type=%EA%B3%B5%EC%A7%80',
-          {
-            headers: {
-              'Content-Type': 'application/json',
-              authorization: `bearer ${lostArkKey}`,
-            },
-          }
-        );
-        const responseData = await response.json();
+        // const response = await fetch(
+        //   'https://developer-lostark.game.onstove.com/news/notices?type=%EA%B3%B5%EC%A7%80',
+        //   {
+        //     headers: {
+        //       'Content-Type': 'application/json',
+        //       authorization: `bearer ${lostArkKey}`,
+        //     },
+        //   }
+        // );
+        // const responseData = await response.json();
+
+        const responseData = await getNotificationList();
 
         const sliceResponseDate = await responseData.slice(0, 5);
 
@@ -55,10 +57,8 @@ const LeftAside = () => {
     // 모로아 공지사항
     const loadMoloaNoti = async () => {
       try {
-        const response = await fetch(MoloaNotiUrl);
-        const responseData = await response.json();
-
-        setMoloaNoti(responseData);
+        const data = await getFirebaseData('MoloaNoti');
+        setMoloaNoti(data);
         setMoloaIsLoading(false);
       } catch {
         console.log('MoloaNoti error');

2a97c6c38bbb8458ce0543989ea7e48e96fe6a0a
KimEunHye <gpqjr00@naver.com>
2023-09-14 00:16:51 +0900

    Feat: 자주 사용하는 컴포넌트 분리

---
+++ src/components/UI/InnerContent.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const InnerContent = (props) => {
+  return (
+    <InnerContentBox height={props.height}>{props.children}</InnerContentBox>
+  );
+};
+
+export default InnerContent;
+
+const InnerContentBox = styled.div`
+  width: 662px;
+  height: ${(props) => props.height || ''};
+  background: #1e2225;
+  margin: 10px 10px 40px 10px;
+
+  @media ${(props) => props.theme.mobile} {
+    width: 95%;
+    margin: 20px 0;
+    padding: 0;
+  }
+`;

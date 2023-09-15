2ce4f1ee36f93816231dda9c72299a12a1b46614
KimEunHye <gpqjr00@naver.com>
2023-09-15 21:16:04 +0900

    Format: console.log 삭제

--- src/components/Character/Content/Equipment/EquipmentPart/AccessoriesDetail.js
+++ src/components/Character/Content/Equipment/EquipmentPart/AccessoriesDetail.js
@@ -25,7 +25,6 @@ const AccessoriesDetail = ({
 
   const AccessoriesBox = ({ item, index }) => {
     const [showTooltip, setShowTooltip] = useState(false);
-    console.log(item);
 
     return (
       <>

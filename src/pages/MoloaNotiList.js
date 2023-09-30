a3d5ea3965e35e565579a0395d6c1c3023b9448d
AkoIsCat <you@gpqjr00@naver.com>
2023-10-01 00:49:56 +0900

    Design: 모로아 공지사항 목록 페이지 기본 레이아웃 추가

--- src/pages/MoloaNotiList.js
+++ src/pages/MoloaNotiList.js
@@ -1,5 +1,25 @@
+import styled from 'styled-components';
+
+import Background from '../components/UI/BackBox';
+import Header from '../components/Header/Header';
+import { Container } from './Home';
+import Footer from '../components/UI/Footer';
+
 const MoloaNotiList = () => {
-  return;
+  return (
+    <Background>
+      <Header />
+      <ContainerBox>모로아 공지사항 목록</ContainerBox>
+      <Footer />
+    </Background>
+  );
 };
 
 export default MoloaNotiList;
+
+const ContainerBox = styled(Container)`
+  width: 100%;
+  min-height: 75vh;
+  height: auto;
+  position: relative;
+`;

4249a56e199237f6fde56340facf7527189b74cc
KimEunHye <gpqjr00@naver.com>
2023-10-02 23:13:34 +0900

    Feat: moloaNoti 데이터 요청 및 렌더링

--- src/pages/MoloaNotiList.js
+++ src/pages/MoloaNotiList.js
@@ -1,15 +1,28 @@
 import styled from 'styled-components';
+import { useQuery } from 'react-query';
+import { getFirebaseData } from '../api/Firebase/FirebaseAxios';
 
 import Background from '../components/UI/BackBox';
 import Header from '../components/Header/Header';
 import { Container } from './Home';
 import Footer from '../components/UI/Footer';
+import Loading from '../components/UI/Loading';
 
 const MoloaNotiList = () => {
+  const { data, isLoading } = useQuery('notification', () =>
+    getFirebaseData('MoloaNoti')
+  );
+
   return (
     <Background>
       <Header />
-      <ContainerBox>모로아 공지사항 목록</ContainerBox>
+      <ContainerBox>
+        {isLoading && <Loading />}
+        <ul>
+          {!isLoading &&
+            data.reverse().map((data) => <li key={data.id}>{data.Title}</li>)}
+        </ul>
+      </ContainerBox>
       <Footer />
     </Background>
   );

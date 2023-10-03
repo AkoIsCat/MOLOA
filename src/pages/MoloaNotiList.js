7b7526789cc0945abe82390a3c5c655eba9265d7
KimEunHye <gpqjr00@naver.com>
2023-10-03 18:06:21 +0900

    Design: UI 변경 및 쿼리 키 변경

--- src/pages/MoloaNotiList.js
+++ src/pages/MoloaNotiList.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { useQuery } from 'react-query';
 import { getFirebaseData } from '../api/Firebase/FirebaseAxios';
+import { useNavigate } from 'react-router-dom';
 
 import Background from '../components/UI/BackBox';
 import Header from '../components/Header/Header';
@@ -9,8 +10,14 @@ import Footer from '../components/UI/Footer';
 import Loading from '../components/UI/Loading';
 
 const MoloaNotiList = () => {
-  const { data, isLoading } = useQuery('notification', () =>
-    getFirebaseData('MoloaNoti')
+  const navigate = useNavigate();
+
+  const { data, isLoading } = useQuery(
+    'moloaNotification',
+    () => getFirebaseData('MoloaNoti'),
+    {
+      refetchOnWindowFocus: false,
+    }
   );
 
   return (
@@ -18,10 +25,21 @@ const MoloaNotiList = () => {
       <Header />
       <ContainerBox>
         {isLoading && <Loading />}
-        <ul>
-          {!isLoading &&
-            data.reverse().map((data) => <li key={data.id}>{data.Title}</li>)}
-        </ul>
+        <PageTitle>공지사항</PageTitle>
+        {data !== undefined && (
+          <List>
+            {!isLoading &&
+              data.reverse().map((item, index) => (
+                <ListItem
+                  key={item.id}
+                  end={index === data.length - 1 ? 'yes' : 'no'}
+                  onClick={() => navigate(`/noti/${item.id}`)}
+                >
+                  {item.Title}
+                </ListItem>
+              ))}
+          </List>
+        )}
       </ContainerBox>
       <Footer />
     </Background>
@@ -35,4 +53,32 @@ const ContainerBox = styled(Container)`
   min-height: 75vh;
   height: auto;
   position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const PageTitle = styled.h2`
+  color: #fff;
+`;
+
+const List = styled.ul`
+  width: 60%;
+  height: auto;
+  list-style: none;
+  padding: 0;
+  margin: 10px 50px;
+  background: #252b2e;
+  border-radius: 20px;
+`;
+
+const ListItem = styled.li`
+  cursor: pointer;
+  border-bottom: ${(props) =>
+    props.end !== 'yes' ? '1px solid #fff' : 'none'};
+  margin: 10px 20px;
+  padding: 10px 20px;
+  color: #fff;
+  font-family: 'Nanum Gothic';
+  font-size: 20px;
 `;

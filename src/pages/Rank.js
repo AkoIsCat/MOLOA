3169388b9a809a24f1041cf104d5342e0fad5d46
KimEunHye <gpqjr00@naver.com>
2023-10-10 02:26:25 +0900

    Chore: useEffect 삭제

--- src/pages/Rank.js
+++ src/pages/Rank.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { getFirebaseData } from '../api/Firebase/FirebaseAxios';
 import { useQuery } from 'react-query';
 

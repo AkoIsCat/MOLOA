d854a213b4dbc7f2ba315f17cea2e629f850e1bc
KimEunHye <gpqjr00@naver.com>
2023-10-11 04:03:11 +0900

    Feat: 데이터가 아직 들어오지 않았을 시 로딩스피너 추가

--- src/components/Character/Content/Equipment.js
+++ src/components/Character/Content/Equipment.js
@@ -2,17 +2,24 @@ import Characteristics from './Equipment/Characteristics';
 import CharacterEquipmentPart from './Equipment/EquipmentPart/CharacterEquipmentPart';
 import CharacterGemsPart from './Equipment/CharacterGemsPart';
 import CharacterCards from './Equipment/CharacterCards';
+import Loading from '../../UI/Loading';
 
 import React from 'react';
 import styled from 'styled-components';
 
 const Equipment = ({
   equipment,
+  equipmentIsLoading,
   engraving,
+  engravingIsLoading,
   gems,
+  gemsIsLoading,
   combatSkills,
+  combatSkillsIsLoading,
   profile,
+  profileIsLoading,
   cards,
+  cardsIsLoading,
   getGems,
 }) => {
   const getGemsList = (data) => {
@@ -21,14 +28,26 @@ const Equipment = ({
 
   return (
     <Wrap>
-      <CharacterEquipmentPart equipment={equipment} engraving={engraving} />
-      <CharacterGemsPart gems={gems} getGemsList={getGemsList} />
-      <Characteristics
-        combatSkills={combatSkills}
-        profile={profile}
-        engraving={engraving}
-      />
-      <CharacterCards cards={cards} />
+      {equipmentIsLoading && engravingIsLoading ? (
+        <Loading />
+      ) : (
+        <CharacterEquipmentPart equipment={equipment} engraving={engraving} />
+      )}
+      {gemsIsLoading ? (
+        <Loading />
+      ) : (
+        <CharacterGemsPart gems={gems} getGemsList={getGemsList} />
+      )}
+      {combatSkillsIsLoading && profileIsLoading && engravingIsLoading ? (
+        <Loading />
+      ) : (
+        <Characteristics
+          combatSkills={combatSkills}
+          profile={profile}
+          engraving={engraving}
+        />
+      )}
+      {cardsIsLoading ? <Loading /> : <CharacterCards cards={cards} />}
     </Wrap>
   );
 };

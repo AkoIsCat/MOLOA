ec969f84c16238e5eae294f7c7ed607587588e1d
KimEunHye <gpqjr00@naver.com>
2023-10-12 23:12:44 +0900

    Fix: 새 컴포넌트 적용 및 key 값 변경

---
+++ src/components/Character/Content/Equipment/CharacterGemsPart/CharacterGemsPart.js
@@ -0,0 +1,96 @@
+import styled from 'styled-components';
+import React from 'react';
+
+import GemsItemTooltip from './GemsItemTooltip';
+
+const CharacterGemsPart = ({ gems, getGemsList }) => {
+  function extractingGemsEffect(gems) {
+    const gemsEffect = [];
+    const effectRegex = /<FONT COLOR='#[^>]+>([^<]+)<\/FONT>\s*([^\n]+)/;
+    const gemsRegex = /<FONT[^>]*>(.*?)<\/FONT>/;
+
+    for (let i = 0; i <= gems.Gems.length - 1; i++) {
+      const tooltipObject = JSON.parse(gems.Gems[i].Tooltip);
+
+      for (let key in tooltipObject) {
+        if (tooltipObject[key].type === 'ItemPartBox') {
+          const matchValue =
+            tooltipObject[key].value['Element_001'].match(effectRegex);
+          const skillName = matchValue[1];
+          const skillShame = matchValue[2];
+          gemsEffect.push({
+            level: gems.Gems[i].Level,
+            name: gems.Gems[i].Name.match(gemsRegex)[1],
+            grade: gems.Gems[i].Grade,
+            skillName,
+            skillShame,
+            icon: gems.Gems[i].Icon,
+          });
+        }
+      }
+    }
+    return gemsEffect;
+  }
+
+  function devideAndSortGems(gems) {
+    const extinction = [];
+    const prominence = [];
+    for (let i = 0; i < gems.length; i++) {
+      if (gems[i].name.includes('멸화')) {
+        extinction.push(gems[i]);
+      } else {
+        prominence.push(gems[i]);
+      }
+      // 보석 레벨 순으로 정렬
+      extinction.sort((a, b) => b.level - a.level);
+      prominence.sort((a, b) => b.level - a.level);
+    }
+
+    return [extinction, prominence];
+  }
+
+  const extractedGems = gems !== null && extractingGemsEffect(gems);
+  const [extinction, prominence] = devideAndSortGems(extractedGems);
+
+  if (extractedGems.length > 1) {
+    getGemsList(extractedGems);
+  }
+
+  return (
+    <ContentWrap>
+      <GemsFlex>
+        {extinction.map((item) => (
+          <GemsItemTooltip key={`${item.skillName} ${item.name}`} item={item} />
+        ))}
+      </GemsFlex>
+      <GemsFlex>
+        {prominence.map((item) => (
+          <GemsItemTooltip key={`${item.skillName} ${item.name}`} item={item} />
+        ))}
+      </GemsFlex>
+    </ContentWrap>
+  );
+};
+
+export default CharacterGemsPart;
+
+const ContentWrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  background: #181c1e;
+  border-radius: 10px;
+  padding: 10px 0 35px 0px;
+  margin: 20px 0 0 0;
+
+  @media ${(props) => props.theme.mobile} {
+    flex-direction: column;
+  }
+`;
+
+const GemsFlex = styled.div`
+  display: flex;
+  margin: 10px 15px;
+  justify-content: center;
+  flex-wrap: wrap;
+`;

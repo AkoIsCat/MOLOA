35bd9263375f1d58b85bb20be12e3c3d0281d5dc
KimEunHye <gpqjr00@naver.com>
2023-11-09 22:06:12 +0900

    Feat: store에 class 추가

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import serverSlice from './modules/serverSlice';
+import classSlice from './modules/classSlice';
 
 const store = configureStore({
   reducer: {
     server: serverSlice.reducer,
+    class: classSlice.reducer,
   },
 });
 

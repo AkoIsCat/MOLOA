61e18627f2e56c23798bfecc36a7e3e47898a960
KimEunHye <gpqjr00@naver.com>
2023-11-09 22:05:51 +0900

    Feat: class 모듈 생성

---
+++ src/redux/modules/classSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  class: '전체',
+  classNumber: undefined,
+};
+
+const classSlice = createSlice({
+  name: 'class',
+  initialState,
+  reducers: {
+    SELECT_CLASS: (state, action) => {
+      return {
+        ...state,
+        class:
+          state.class !== action.payload.class ? action.payload.class : '전체',
+        classNumber:
+          state.classNumber !== action.payload.classNumber
+            ? action.payload.classNumber
+            : undefined,
+      };
+    },
+  },
+});
+
+export const { SELECT_CLASS } = classSlice.actions;
+
+export default classSlice;

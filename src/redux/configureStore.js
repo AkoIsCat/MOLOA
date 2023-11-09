0ae520cea4663e98bfaed12e20f651e90855c5ae
KimEunHye <gpqjr00@naver.com>
2023-11-09 23:13:22 +0900

    Feat: sotre에 jobEngraving 추가

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import serverSlice from './modules/serverSlice';
 import classSlice from './modules/classSlice';
+import jobEngravingSlice from './modules/jobEngravingSlice';
 
 const store = configureStore({
   reducer: {
     server: serverSlice.reducer,
     class: classSlice.reducer,
+    jobEngraving: jobEngravingSlice.reducer,
   },
 });
 

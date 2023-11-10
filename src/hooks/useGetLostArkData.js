654a707f5123a446d3fb016fbdb2cebd87e6f4d8
KimEunHye <gpqjr00@naver.com>
2023-11-11 02:16:59 +0900

    Feat: lostark 요청에 대한 custom hook 생성

---
+++ src/hooks/useGetLostArkData.js
@@ -0,0 +1,41 @@
+import { useQuery } from 'react-query';
+
+export const useGetLostArkData = (
+  queryKey,
+  id,
+  queryFn,
+  staleTimeValue,
+  selectFn
+) => {
+  const { data, isLoading, refetch, dataUpdatedAt } = useQuery(
+    [`${queryKey}`, id],
+    () => queryFn(id),
+    {
+      enabled: !!id,
+      refetchOnWindowFocus: false,
+      staleTime: staleTimeValue,
+      select: (data) => (selectFn ? selectFn(data) : data),
+    }
+  );
+
+  return { data, isLoading, refetch, dataUpdatedAt };
+};
+
+export const useGetLostArkDataNotId = (
+  queryKey,
+  queryFn,
+  staleTimeValue,
+  selectFn
+) => {
+  const { data, isLoading, refetch, dataUpdatedAt } = useQuery(
+    [`${queryKey}`],
+    () => queryFn(),
+    {
+      refetchOnWindowFocus: false,
+      staleTime: staleTimeValue,
+      select: (data) => (selectFn ? selectFn(data) : data),
+    }
+  );
+
+  return { data, isLoading, refetch, dataUpdatedAt };
+};

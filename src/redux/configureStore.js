b0af21a7f7ffbbd6ddb8c99740eda12bcf53f4b8
김은혜 <gpqjr00@naver.com>
2023-11-11 03:15:45 +0900

    Feat: store에 guild 추가

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import serverSlice from './modules/serverSlice';
 import classSlice from './modules/classSlice';
 import jobEngravingSlice from './modules/jobEngravingSlice';
+import guildSlice from './modules/guildSlice';
 
 const store = configureStore({
   reducer: {
     server: serverSlice.reducer,
     class: classSlice.reducer,
     jobEngraving: jobEngravingSlice.reducer,
+    guild: guildSlice.reducer,
   },
 });
 

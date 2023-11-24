0f7f25193771aa801862753bd3ceb6579570bb2c
KimEunHye <gpqjr00@naver.com>
2023-11-25 01:28:22 +0900

    Design: Title css 수정

--- src/components/UI/SignTitle.js
+++ src/components/UI/SignTitle.js
@@ -8,6 +8,7 @@ export default SignTitle;
 
 const Title = styled.p`
   width: ${(props) => (props.size === 'small' ? '70px' : '140px')};
+  margin: 0;
   margin-right: 10px;
   font-size: 16px;
   color: #c1c1c1;

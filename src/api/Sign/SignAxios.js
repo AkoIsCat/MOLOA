e12f712d7b91c17e49ae47ca84e4edfe5d95f218
KimEunHye <gpqjr00@naver.com>
2023-11-27 00:33:18 +0900

    Feat: 회원가입 함수 생성

--- src/api/Sign/SignAxios.js
+++ src/api/Sign/SignAxios.js
@@ -2,10 +2,22 @@ import { SignInstance } from './SignInstance';
 
 export const postSignUpData = async (data) => {
   try {
-    console.log(data);
     const response = await SignInstance().post(`/signup`, data);
-    return response;
-  } catch {
-    throw new Error('Sign Request error');
+    if (response.status === 201) {
+      return response.data;
+    }
+  } catch (error) {
+    return error.response.data;
+  }
+};
+
+export const signInData = async (data) => {
+  try {
+    const response = await SignInstance().post('/login', data);
+    if (response.status === 200) {
+      return response.data;
+    }
+  } catch (error) {
+    return error.response.data;
   }
 };

b1333b9a141c15d7e9b602ebfca864a9b24f59b3
KimEunHye <gpqjr00@naver.com>
2023-11-27 05:40:49 +0900

    Feat: state로 리렌더링 시점을 제어하기 위해 로그인과 로그아웃 함수를 페이지 컴포넌트에 생성

--- src/pages/Community.js
+++ src/pages/Community.js
@@ -6,19 +6,37 @@ import { Container } from './Home';
 import { Message } from './Character';
 import SignIn from '../components/Community/SignIn';
 import { useEffect, useState } from 'react';
-import { getNickname } from '../api/Sign/SignAxios';
+import { getNickname, signInData } from '../api/Sign/SignAxios';
 
 const Coummunity = () => {
   const toggle = false;
   const [nickname, setNickname] = useState(undefined);
 
+  const getNicknameData = async () => {
+    const response = await getNickname();
+    setNickname(response.nickname);
+  };
+
   useEffect(() => {
-    const getNicknameData = async () => {
-      const response = await getNickname();
-      setNickname(response.nickname);
-    };
     toggle && getNicknameData();
-  }, [toggle]);
+  }, [toggle, nickname]);
+
+  const onClickSignIn = async (data) => {
+    const response = await signInData(data);
+    if (response.success) {
+      localStorage.setItem('userId', data.id);
+      const nicknameRes = await getNickname();
+      setNickname(nicknameRes.nickname);
+      alert('로그인에 성공하였습니다.');
+    } else if (response.data.error) {
+      alert('아이디나 비밀번호가 일치하지 않습니다.');
+    }
+  };
+
+  const onClickLogout = () => {
+    localStorage.removeItem('userId');
+    setNickname(undefined);
+  };
 
   return (
     <Background>
@@ -28,7 +46,11 @@ const Coummunity = () => {
         {toggle && (
           <>
             <Side>
-              <SignIn nickname={nickname} />
+              <SignIn
+                nickname={nickname}
+                onClickLogout={onClickLogout}
+                onClickSignIn={onClickSignIn}
+              />
             </Side>
             <Section></Section>
           </>

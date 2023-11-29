94e19742b3b216fc9a6fb4cf94dcb04098316a22
KimEunHye <gpqjr00@naver.com>
2023-11-29 18:54:58 +0900

    Feat: 글쓰기, 목록 버튼 css 추가

--- src/components/UI/SignButton.js
+++ src/components/UI/SignButton.js
@@ -3,7 +3,11 @@ import styled from 'styled-components';
 const SignButton = (props) => {
   return (
     <SignInBtn
-      disabled={!(props.idStatus.status && props.pwStatus.status)}
+      disabled={
+        props.name === '글쓰기' || props.name === '목록'
+          ? false
+          : !(props.idStatus.status && props.pwStatus.status)
+      }
       name={props.name}
       onClick={props.onClick}
     >
@@ -15,18 +19,20 @@ const SignButton = (props) => {
 export default SignButton;
 
 const SignInBtn = styled.button`
-  width: 85%;
-  height: ${(props) => (props.name === '로그인' ? '30px' : '50px')};
+  width: ${(props) =>
+    props.name === '글쓰기' || props.name === '목록' ? '8%' : '85%'};
+  height: ${(props) =>
+    props.name === '로그인' ? '30px' : props === '회원가입' ? '50px' : '40px'};
   border-radius: 10px;
   border: 0;
-  margin: 5px 0;
+  margin: 5px;
   cursor: pointer;
   font-family: 'Nanum Gothic';
 
   &:enabled {
-    background: skyblue;
+    background: ${(props) => (props.name === '목록' ? '#6D7276' : 'skyblue')};
     &:hover {
-      background: #358ed0;
+      background: ${(props) => (props.name === '목록' ? '#4B535A' : '#358ed0')};
     }
   }
 `;

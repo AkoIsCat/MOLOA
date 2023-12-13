574a2f389bb7cd94edf68590daab408933287563
KimEunHye <gpqjr00@naver.com>
2023-12-14 05:24:59 +0900

    Feat: 댓글 관련 css 추가

--- src/pages/PostsDetail.js
+++ src/pages/PostsDetail.js
@@ -80,17 +80,35 @@ const PostsDetail = () => {
       <ContainerBox>
         <Side />
         <ContentsWrap>
-          <DetailTable
-            data={data}
-            isLoading={isLoading}
-            onClickLike={onClickLike}
-          />
-          <ButtonWrap>
-            <PostsButton onClick={() => navigate('/community')} name="목록" />
-            {isItSameId && <PostsButton name="수정" onClick={onClickModify} />}
-            {isItSameId && <PostsButton name="삭제" onClick={onClickRemove} />}
-            <PostsButton name="글쓰기" onClick={onClickWrite} />
-          </ButtonWrap>
+          <section>
+            <DetailTable
+              data={data}
+              isLoading={isLoading}
+              onClickLike={onClickLike}
+            />
+            <ButtonWrap>
+              <PostsButton onClick={() => navigate('/community')} name="목록" />
+              {isItSameId && (
+                <PostsButton name="수정" onClick={onClickModify} />
+              )}
+              {isItSameId && (
+                <PostsButton name="삭제" onClick={onClickRemove} />
+              )}
+              <PostsButton name="글쓰기" onClick={onClickWrite} />
+            </ButtonWrap>
+          </section>
+          <section>
+            <div>
+              <p>댓글 {data.post.comment_count}개</p>
+            </div>
+            <div>댓글 리스트</div>
+            <form>
+              <Textarea
+                placeholder="타인의 권리를 침해하거나 명예를 훼손하는 댓글은 법적으로 문제가 될 수 있습니다.&#10;Shift+Enter 키를 동시에 누르면 줄바꿈이 됩니다."
+              ></Textarea>
+              <button>등록</button>
+            </form>
+          </section>
         </ContentsWrap>
       </ContainerBox>
       <Footer />
@@ -116,4 +134,18 @@ const ButtonWrap = styled.div`
   margin: 10px 45px;
   display: flex;
   justify-content: flex-end;
+`;
+
+const Textarea = styled.textarea`
+  width: 85%;
+  min-height: 80px;
+  margin: 20px 0px;
+  padding: 10px;
+  font-size: 14px;
+  outline: none;
+  background: #1e2225;
+  color: #fff;
+  border: 1px solid #c1c1c1;
+  border-radius: 10px;
+  resize: vertical;
 `;

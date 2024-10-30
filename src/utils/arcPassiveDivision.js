0d44821b2b34ad6b053e506809b6bde7536197b7
KimEunHye <gpqjr00@naver.com>
2024-10-30 19:07:58 +0900

    Comment: 불필요한 주석 삭제 및 주석 작성

--- src/utils/arcPassiveDivision.js
+++ src/utils/arcPassiveDivision.js
@@ -1,18 +1,9 @@
 export const arcPassiveDivision = (tooltip) => {
   let arcPassiveActivate = 0;
-  console.log(tooltip[2].effect);
+  // tooltip 초반 인덱스는 내용이 고정 항목이기 때문에 고정 항목을 가지고 아크패시브 활성화 여부를 체크
   !tooltip[2].effect?.includes('진화')
     ? (arcPassiveActivate = 0)
     : (arcPassiveActivate = 1);
-
-  // for (let obj of tooltip) {
-  //   for (let property in obj) {
-  //     if (property === 'itemLevel') {
-  //       const itemInfo = obj[property];
-  //       itemInfo.includes('티어 3') ? (tier = 3) : (tier = 4);
-  //     }
-  //   }
-  // }
 
   return arcPassiveActivate;
 };

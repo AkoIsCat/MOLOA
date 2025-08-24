be99d8505d6e185deefa1484ed64ea10076af182
KimEunHye <gpqjr00@naver.com>
2025-08-25 02:01:09 +0900

    Feat: ItemTitle 추출 함수 작성

---
+++ src/utils/equipment/getItemTitle.js
@@ -0,0 +1,9 @@
+import { findByType } from '../findBtType';
+
+// ItemTitle 데이터 (품질)
+export const getItemTitle = (data) => {
+  const findData = findByType(data, 'ItemTitle');
+  const qualityValue = findData[0]?.value?.qualityValue;
+
+  return qualityValue;
+};

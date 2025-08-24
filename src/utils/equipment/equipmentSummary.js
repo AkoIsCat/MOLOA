550ec76c2e11606b0dd7754ff4061bac7e81290c
KimEunHye <gpqjr00@naver.com>
2025-08-25 02:02:00 +0900

    Comment: 불필요한 주석 제거

--- src/utils/equipment/equipmentSummary.js
+++ src/utils/equipment/equipmentSummary.js
@@ -2,8 +2,6 @@ import { parseTooltips } from '../parseTooltips';
 import { mapEquipmentPart } from './mapEquipmentPart';
 
 export const equipmentSummary = (data) => {
-  console.log('equipment input data', data);
-
   const tooltips = parseTooltips(data);
 
   return data.map((data, idx) => mapEquipmentPart(data, idx, tooltips));

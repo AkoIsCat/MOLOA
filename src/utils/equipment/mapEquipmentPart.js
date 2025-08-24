693eea682566df99410ca74377b197f7dffa57f0
KimEunHye <gpqjr00@naver.com>
2025-08-25 02:01:47 +0900

    Feat: 품질 항목 추가

--- src/utils/equipment/mapEquipmentPart.js
+++ src/utils/equipment/mapEquipmentPart.js
@@ -1,12 +1,14 @@
 import { getPartBoxes } from './getPartBoxes';
 import { getIdenStringGroups } from './getIdenStringGroups';
 import { getSingleTextBox } from './getSingleTextBox';
+import { getItemTitle } from './getItemTitle';
 
 export const mapEquipmentPart = (fullData, idx, tooltips) => {
   const data = tooltips[idx] || {};
   const PartBoxesBoxes = getPartBoxes(data);
   const IndentStringBoxes = getIdenStringGroups(data);
   const SingleTextBox = getSingleTextBox(data);
+  const ItemTitleBox = getItemTitle(data);
 
   return {
     PartName: fullData.Type,
@@ -15,5 +17,6 @@ export const mapEquipmentPart = (fullData, idx, tooltips) => {
     PartBoxes: PartBoxesBoxes.map((pb) => pb.Element_001),
     IndentStrings: IndentStringBoxes,
     SingleText: SingleTextBox,
+    ItemTitle: ItemTitleBox,
   };
 };

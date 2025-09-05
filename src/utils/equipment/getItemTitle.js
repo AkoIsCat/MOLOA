aea87d7752e8ee6d949998be1aca4df70f818398
KimEunHye <gpqjr00@naver.com>
2025-09-05 23:51:57 +0900

    Feat: 아이템 레벨부분 가공

--- src/utils/equipment/getItemTitle.js
+++ src/utils/equipment/getItemTitle.js
@@ -1,9 +1,13 @@
 import { findByType } from '../findByType';
+import removeTag from '../removeTag';
 
 // ItemTitle 데이터 (품질)
 export const getItemTitle = (data) => {
   const findData = findByType(data, 'ItemTitle');
   const qualityValue = findData[0]?.value?.qualityValue;
+  const itemLevel = removeTag(findData[0]?.value?.leftStr2, 'FONT').split(
+    ' '
+  )[2];
 
-  return qualityValue;
+  return { qualityValue, itemLevel };
 };

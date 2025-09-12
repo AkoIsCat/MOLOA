86e5474cb2896155220169d641efb3efccf0e158
KimEunHye <gpqjr00@naver.com>
2025-09-13 01:06:27 +0900

    Fix: 각 부위별 활성화된 초월 갯수를 가져오도록 수정

--- src/utils/equipment/getIdenStringGroups.js
+++ src/utils/equipment/getIdenStringGroups.js
@@ -4,11 +4,15 @@ import removeTag from '../removeTag';
 // IdenStringGroups 데이터 추출(초월, 엘릭서)
 export const getIdenStringGroups = (data) => {
   const findData = findByType(data, 'IndentStringGroup');
+  console.log(findData);
   const extractDesiredValues = findData
     .map((n) => {
       const content = n?.value?.Element_000;
       const IdenStringGroup1 = content?.contentStr?.Element_000?.contentStr;
       const IdenStringGroup2 = content?.contentStr?.Element_001?.contentStr;
+      const Transcendence = content?.topStr.includes('초월')
+        ? content?.topStr
+        : '';
 
       return {
         IdenStringGroup1: removeTag(
@@ -19,6 +23,7 @@ export const getIdenStringGroups = (data) => {
           removeTag(removeTag(IdenStringGroup2, 'FONT'), 'font'),
           'img'
         ),
+        Transcendence,
       };
     })
     .filter(Boolean);

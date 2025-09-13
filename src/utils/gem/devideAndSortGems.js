ae73359d290f905975e2fd82a42bf655dd7d6ca6
KimEunHye <gpqjr00@naver.com>
2025-09-14 02:30:43 +0900

    Feat: 보석의 딜증과 쿨감을 나누는 유틸함수 작성

---
+++ src/utils/gem/devideAndSortGems.js
@@ -0,0 +1,20 @@
+export function devideAndSortGems(gems) {
+  console.log(gems);
+  const extinction = [];
+  const prominence = [];
+  for (let i = 0; i < gems.length; i++) {
+    if (
+      gems[i].GemEffect.includes('피해') ||
+      gems[i].GemName.includes('겁화')
+    ) {
+      extinction.push(gems[i]);
+    } else {
+      prominence.push(gems[i]);
+    }
+    // 보석 레벨 순으로 정렬
+    extinction.sort((a, b) => b.Level - a.Level); // 피증
+    prominence.sort((a, b) => b.Level - a.Level); // 재사용
+  }
+
+  return [extinction, prominence];
+}

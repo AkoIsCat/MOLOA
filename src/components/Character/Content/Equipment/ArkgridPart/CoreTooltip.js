9dabe12ff8829795dd9848d298e080bfb1cf1574
KimEunHye <gpqjr00@naver.com>
2025-09-19 00:53:16 +0900

    Chore: 불필요한 console.log 삭제

--- src/components/Character/Content/Equipment/ArkgridPart/CoreTooltip.js
+++ src/components/Character/Content/Equipment/ArkgridPart/CoreTooltip.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 const CoreTooltip = ({ item }) => {
   const [showTooltip, setShowTooltip] = useState(false);
-  console.log(item);
+
   return (
     <EffectWrap>
       {showTooltip && (

7de38792c4af65796e91bc9dadf18e01a4488f21
KimEunHye <gpqjr00@naver.com>
2025-09-18 23:51:14 +0900

    Feat: 엘릭서 합계 추가 및 사용하지 않는 컴포넌트 삭제

--- src/components/Character/Content/Equipment/EquipmentPart/MountedEngraving.js
+++ src/components/Character/Content/Equipment/EquipmentPart/MountedEngraving.js
@@ -1,21 +1,29 @@
 import styled from 'styled-components';
 import Transcendence_icon from '../../../../../asset/icon/transcendence_icon.png';
 
-const MountedEngraving = ({ elixirTotalLevel, transcendenceTotal }) => {
-  // console.log(transcendenceTotal);
+const MountedEngraving = ({ elixirTotal, transcendenceTotal }) => {
+  const total =
+    elixirTotal !== null &&
+    elixirTotal
+      .map((v) => Number(v.ElixirTotal)) // 숫자로 변환
+      .filter((v) => !isNaN(v)) // NaN 제거
+      .reduce((acc, cur) => acc + cur, 0);
+
   return (
     <MountWrap>
-      <EffectTotal>
-        <img
-          src="https://firebasestorage.googleapis.com/v0/b/lostark-bf0ba.appspot.com/o/%EC%97%98%EB%A6%AD%EC%84%9C.png?alt=media&token=eb19bb1a-1aa2-48d5-8383-cf67336ac49f"
-          alt="엘릭서 이미지"
-          width="14"
-          height="17"
-        />
-        <div className="transcendence">엘릭서 연성 레벨 합 </div>
-        {/* <div className="elixir"></div> */}
-        <div className="activeExlixir">11</div>
-      </EffectTotal>
+      {/* {엘릭서 합계} */}
+      {elixirTotal !== null && (
+        <EffectTotal>
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/lostark-bf0ba.appspot.com/o/%EC%97%98%EB%A6%AD%EC%84%9C.png?alt=media&token=eb19bb1a-1aa2-48d5-8383-cf67336ac49f"
+            alt="엘릭서 이미지"
+            width="14"
+            height="17"
+          />
+          <div className="transcendence">엘릭서 연성 레벨 합 </div>
+          <div className="activeExlixir">{total}</div>
+        </EffectTotal>
+      )}
       {/*초월 합계*/}
       <EffectTotal>
         {transcendenceTotal && (
@@ -39,59 +47,6 @@ const MountedEngraving = ({ elixirTotalLevel, transcendenceTotal }) => {
 
 export default MountedEngraving;
 
-// const MountedEngravingBox = styled.div`
-//   display: flex;
-//   align-items: center;
-//   width: auto;
-
-//   @media ${(props) => props.theme.mobile} {
-//     margin: 10px 0;
-//   }
-
-//   img {
-//     border-radius: 50%;
-//     width: 50px;
-//     height: 50px;
-//     object-fit: cover;
-//     margin-right: 5px;
-//   }
-
-//   div {
-//     width: 98px;
-//     margin: 0 15px 0 0;
-//     display: flex;
-//     flex-direction: column;
-
-//     p {
-//       font-size: 15px;
-//       margin: 0 0px 5px 5px;
-//       font-family: 'Nanum Gothic';
-//       color: #fff;
-//     }
-
-//     @media ${(props) => props.theme.mobile} {
-//       .name {
-//         font-size: 13px;
-//       }
-//     }
-
-//     .name {
-//       width: 100px;
-//       margin: 5px;
-//       color: ${(props) =>
-//         props.grade === '+12'
-//           ? '#fe9600'
-//           : props.grade === ' +9'
-//           ? '#9e24ca'
-//           : props.grade === ' +6'
-//           ? '#113d5d'
-//           : props.grade === ' +3'
-//           ? '#46812d'
-//           : ''};
-//     }
-//   }
-// `;
-
 const MountWrap = styled.div`
   min-width: 200px;
   max-width: 312px;
@@ -106,7 +61,8 @@ const MountWrap = styled.div`
     width: 80%;
     justify-content: center;
     align-items: center;
-    margin-top: 30px;
+    margin: 0 auto;
+    margin-bottom: 30px;
     padding: 0;
   }
 `;
@@ -132,43 +88,3 @@ const EffectTotal = styled.div`
     margin: 0 5px;
   }
 `;
-
-// const ArkPassive = styled.div`
-//   display: flex;
-//   flex-flow: column;
-//   margin: 0;
-//   font-size: 16px;
-//   color: #7053c3;
-
-//   .title {
-//     margin-left: 20px;
-//     align-items: center;
-
-//     div {
-//       margin-top: 8px;
-//       font-weight: bold;
-//     }
-//   }
-
-//   img {
-//     width: 40px;
-//     height: 40px;
-//     margin: 0 10px;
-//   }
-// `;
-
-// const ArkPassivePoint = styled.div`
-//   display: flex;
-//   margin: 0 10px;
-//   font-size: 18px;
-
-//   .point_type {
-//     margin: 0px 3px;
-//     color: ${(props) =>
-//       props.name === '진화'
-//         ? '#F1D594'
-//         : props.name === '깨달음'
-//         ? '#83E9FF'
-//         : '#C2EA55'};
-//   }
-// `;
